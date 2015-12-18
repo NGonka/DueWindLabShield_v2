cb36b90d1bf3eaa957d02dd34ae3ca7eaaa640f4
NGonka <ngonka@gmx.de>
2015-12-18 13:07:31 +0100

    beta version of the WIndLabShield firmware

---
+++ __vm/.DueWindLabShieldv2.vsarduino.h
@@ -0,0 +1,71 @@
+/* 
+	Editor: http://www.visualmicro.com
+	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
+	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+	
+	Hardware: Arduino Due (Programming Port), Platform=sam, Package=arduino
+*/
+
+#define ARDUINO 166
+#define ARDUINO_MAIN
+#define F_CPU 84000000L
+#define printf iprintf
+#define __SAM__
+#define printf iprintf
+#define F_CPU 84000000L
+#define ARDUINO 166
+#define ARDUINO_SAM_DUE
+#define ARDUINO_ARCH_SAM
+#define __SAM3X8E__
+#define USB_VID 0x2341
+#define USB_PID 0x003e
+#define USBCON
+extern "C" void __cxa_pure_virtual() {;}
+
+double readVcc();
+long readVoltage(double Vcc_bit);
+double corrCurrent(double Current);
+long readCurrent(double Vcc_bit);
+void L_setLoad(uint16_t value);
+uint16_t L_perMille2bit(int16_t _perMille);
+void P_resetPitchPosition(void);
+int32_t P_Distance_to_go();
+void P_Enable(uint8_t activate);
+void P_Step();
+float sind(float _Angle);
+float tand(float _Angle);
+int32_t long P_Angle2Step(int32_t _gamma);
+void P_setMSteps(uint8_t _msteps);
+void Fan(int Speed);
+void M_Brake_active();
+void M_Brake_deactive();
+void M_forward();
+void M_backward();
+void M_Brake();
+void M_Brake_release();
+void M_SleepMode(int _Mode);
+void M_SwitchMotorModeOff();
+void M_SwitchMotorModeOn();
+void M_SetMotorSpeed(long _speed);
+long M_Percent2bit(int _percent);
+void printHelp();
+int SetCommand(String _msg);
+void TurnOffWindLab(void);
+void PinSetup();
+void G_revProtect(uint8_t protPin);
+//
+//
+
+#include "C:\Users\mw7\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.6\variants\arduino_due_x\pins_arduino.h" 
+#include "C:\Users\mw7\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.6\variants\arduino_due_x\variant.h" 
+#include "C:\Users\mw7\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.6\cores\arduino\arduino.h"
+#include <..\DueWindLabShieldv2\DueWindLabShieldv2.ino>
+#include <..\DueWindLabShieldv2\MCP4726.cpp>
+#include <..\DueWindLabShieldv2\MCP4726.h>
+#include <..\DueWindLabShieldv2\MCP4726_SETUP_VREF.h>
+#include <..\DueWindLabShieldv2\constString.cpp>
+#include <..\DueWindLabShieldv2\constString.h>
+#include <..\DueWindLabShieldv2\speed_enc.cpp>
+#include <..\DueWindLabShieldv2\speed_enc.h>
